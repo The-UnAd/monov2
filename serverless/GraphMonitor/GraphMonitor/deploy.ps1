53b40f55f29119bdf1d9ce75ea52a3750417b656
Stephen Collins <sgcollins@outlook.com>
2024-02-15 16:17:13 -0500

    fixed deploy scripts

--- serverless/GraphMonitor/GraphMonitor/deploy.ps1
+++ serverless/GraphMonitor/GraphMonitor/deploy.ps1
@@ -1,4 +1,3 @@
-
-dotnet lambda deploy-function graph-monitor-authorizer --config-file .\serverless.template --function-runtime dotnet8 --function-role lambda_execution_role --function-memory-size 256 --function-timeout 60 --function-handler GraphMonitor::GraphMonitor.Authorizer_Authorize_Generated::Authorize
-
-dotnet lambda deploy-function graph-monitor-authorizer --config-file .\serverless.template --function-runtime dotnet8 --function-role lambda_execution_role --function-memory-size 256 --function-timeout 60 --function-handler GraphMonitor::GraphMonitor.Authorizer_Authorize_Generated::Authorize
+& .\deploy-graph-monitor-authorizer.ps1
+& .\deploy-graph-monitor-get.ps1
+& .\deploy-graph-monitor-post.ps1
