49205ebc2e38e96cc79cae0375c88ccd976f1b16
Stephen Collins <sgcollins@outlook.com>
2024-02-22 13:55:42 -0500

    cleaned up local docker config

--- start-backend.ps1
+++ start-backend.ps1
@@ -1,5 +1,4 @@
 docker-compose --file .\docker-compose.backend.yml up -d redis
 docker-compose --file .\docker-compose.backend.yml up -d postgres
-docker-compose --file .\docker-compose.backend.yml run --rm db_script
 docker-compose --file .\docker-compose.backend.yml run --rm migrator
 
