0610653c051ae3a6223e17ec9127e6306f8f281c
Stephen Collins <sgcollins@outlook.com>
2024-02-22 18:25:29 -0500

    updated to ef core 8.0.2

--- dotnet/UnAd.Data/Users/Migrations/UserDbContextModelSnapshot.cs
+++ dotnet/UnAd.Data/Users/Migrations/UserDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace UnAd.Data.Users.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.1")
+                .HasAnnotation("ProductVersion", "8.0.2")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.HasPostgresExtension(modelBuilder, "uuid-ossp");
