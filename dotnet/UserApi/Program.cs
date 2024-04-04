10eed4879ccd010a40ad5b9896e103f39fb00625
Stephen Collins <sgcollins@outlook.com>
2024-04-04 11:32:34 -0400

    cleanup

--- dotnet/UserApi/Program.cs
+++ dotnet/UserApi/Program.cs
@@ -34,20 +34,18 @@ builder.Services.AddSingleton(() => {
 
 builder.Services.AddSingleton<IMessageSender, MessageSender>();
 
-if (!builder.Environment.IsDevelopment()) {
-    builder.Services.AddAuthentication(options => {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options => {
-        options.Authority = builder.Configuration["Cognito:Authority"];
-        options.TokenValidationParameters = new TokenValidationParameters {
-            ValidateIssuerSigningKey = true,
-            ValidateAudience = false
-        };
-    });
-    builder.Services.AddAuthorization();
-}
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options => {
+    options.Authority = builder.Configuration["Cognito:Authority"];
+    options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
 
 builder.Services
     .AddGraphQLServer()
