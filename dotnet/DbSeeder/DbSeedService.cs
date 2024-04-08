83f8c2d22b2ccbb18ce3729f2a32fc40ca1f76ed
Stephen Collins <sgcollins@outlook.com>
2024-04-08 18:22:40 -0400

    updated maxMessages handling to be price-specific

--- dotnet/DbSeeder/DbSeedService.cs
+++ dotnet/DbSeeder/DbSeedService.cs
@@ -39,19 +39,29 @@ internal class DbSeedService(ILogger<DbSeedService> logger,
 
     private async Task StoreProducts(CancellationToken cancellationToken) {
         var productService = new ProductService(stripe);
+        var priceService = new PriceService(stripe);
 
-        var products = await productService.ListAsync(new ProductListOptions {
-            Limit = 10, // TODO: how do I decide how many to get?
-            Active = true,
-            Type = "service"
-        }, cancellationToken: cancellationToken);
+        try {
+            var products = await productService.ListAsync(new ProductListOptions {
+                Limit = 10, // TODO: how do I decide how many to get?
+                Active = true,
+                Type = "service"
+            }, cancellationToken: cancellationToken);
 
-        var db = redis.GetDatabase();
-        logger.LogConnectedToRedis(redis.GetEndPoints().First());
-        foreach (var product in products) {
-            db.StoreProduct(product.Id, product.Name, product.Description);
-            db.SetProductLimits(product.Id, product.Metadata);
-            logger.LogStoredProduct(product.Id, product.Name);
+            var db = redis.GetDatabase();
+            logger.LogConnectedToRedis(redis.GetEndPoints().First());
+            foreach (var product in products) {
+                var prices = await priceService.ListAsync(new PriceListOptions {
+                    Product = product.Id
+                }, cancellationToken: cancellationToken);
+                foreach (var price in prices) {
+                    db.StorePrice(price.Id, product.Name, product.Description);
+                    db.SetPriceLimits(price.Id, price.Metadata);
+                    logger.LogStoredPriceLimits(price.Id, product.Name);
+                }
+            }
+        } catch (Exception ex) {
+            logger.LogException(ex);
         }
     }
 
@@ -66,26 +76,39 @@ internal static class DbSeedServiceLogs {
         public const int ServiceStopping = 100;
         public const int ServiceStarting = 101;
         public const int ConnectedToRedis = 200;
-        public const int StoredProduct = 201;
+        public const int StorePrice = 201;
         public const int StoredDefaultClient = 300;
+        public const int Exception = 301;
     }
 
     public static readonly Action<ILogger<DbSeedService>, Exception?> ServiceStopping =
         LoggerMessage.Define(LogLevel.Information, new EventId(EventIds.ServiceStopping, nameof(ServiceStopping)), "DbSeedService is stopping.");
+
     public static readonly Action<ILogger<DbSeedService>, Exception?> ServiceStarting =
         LoggerMessage.Define(LogLevel.Information, new EventId(EventIds.ServiceStarting, nameof(ServiceStarting)), "DbSeedService is starting.");
 
     public static readonly Action<ILogger<DbSeedService>, System.Net.EndPoint, Exception?> ConnectedToRedis =
         LoggerMessage.Define<System.Net.EndPoint>(LogLevel.Information, new EventId(EventIds.ConnectedToRedis, nameof(ConnectedToRedis)), "Connected to Redis at {Endpoint}");
-    public static readonly Action<ILogger<DbSeedService>, string, string, Exception?> StoredProduct =
-        LoggerMessage.Define<string, string>(LogLevel.Information, new EventId(EventIds.StoredProduct, nameof(StoredProduct)), "Stored product {Id}: {Name}");
+
+    public static readonly Action<ILogger<DbSeedService>, string, string, Exception?> StorePrice =
+        LoggerMessage.Define<string, string>(LogLevel.Information, new EventId(EventIds.StorePrice, nameof(StorePrice)), "Stored price {Id}: (Product: {Name})");
 
     public static readonly Action<ILogger<DbSeedService>, Guid, Exception?> StoredDefaultClient =
         LoggerMessage.Define<Guid>(LogLevel.Information, new EventId(EventIds.StoredDefaultClient, nameof(StoredDefaultClient)), "Stored default client with Id {Id}");
 
-    public static void LogServiceStopping(this ILogger<DbSeedService> logger, Exception? ex = null) => ServiceStopping(logger, ex);
-    public static void LogServiceStarting(this ILogger<DbSeedService> logger, Exception? ex = null) => ServiceStarting(logger, ex);
-    public static void LogConnectedToRedis(this ILogger<DbSeedService> logger, System.Net.EndPoint endpoint, Exception? ex = null) => ConnectedToRedis(logger, endpoint, ex);
-    public static void LogStoredProduct(this ILogger<DbSeedService> logger, string id, string name, Exception? ex = null) => StoredProduct(logger, id, name, ex);
-    public static void LogStoredDefaultClient(this ILogger<DbSeedService> logger, Guid id, Exception? ex = null) => StoredDefaultClient(logger, id, ex);
+    public static readonly Action<ILogger<DbSeedService>, Exception?> Exception =
+        LoggerMessage.Define(LogLevel.Error, new EventId(EventIds.Exception, nameof(Exception)), "Error storing price");
+
+    public static void LogServiceStopping(this ILogger<DbSeedService> logger) =>
+        ServiceStopping(logger, null);
+    public static void LogServiceStarting(this ILogger<DbSeedService> logger) =>
+        ServiceStarting(logger, null);
+    public static void LogConnectedToRedis(this ILogger<DbSeedService> logger, System.Net.EndPoint endpoint) =>
+        ConnectedToRedis(logger, endpoint, null);
+    public static void LogStoredPriceLimits(this ILogger<DbSeedService> logger, string id, string name) =>
+        StorePrice(logger, id, name, null);
+    public static void LogStoredDefaultClient(this ILogger<DbSeedService> logger, Guid id) =>
+        StoredDefaultClient(logger, id, null);
+    public static void LogException(this ILogger<DbSeedService> logger, Exception ex) =>
+        Exception(logger, ex);
 }
