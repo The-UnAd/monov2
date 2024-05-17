306ef04c88f2ee76a8fbf4282bd6f9b086c12488
Stephen Collins <sgcollins@outlook.com>
2024-05-16 20:57:14 -0400

    back to old ga loading method

--- web/unad-web/src/pages/_document.page.tsx
+++ web/unad-web/src/pages/_document.page.tsx
@@ -18,15 +18,28 @@ export default function MyDocument() {
         <link rel="apple-touch-icon" href="/img/favicon.webp" />
         <link rel="icon" type="image/x-icon" href="/img/favicon.webp" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <Script id="clarity" src="/js/clarity.js" />
         <Script
-          id="gtag"
           async
           strategy="lazyOnload"
           src="https://www.googletagmanager.com/gtag/js?id=G-L2SWP1F1TF"
         ></Script>
-        <Script id="ga" src="/js/ga.js" />
-        <Script id="tm" src="/js/tm.js" />
+        <Script id="ga">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L2SWP1F1TF');
+          `}
+        </Script>
+        <Script id="clarity">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "mbjvc7zj0r");
+          `}
+        </Script>
       </Head>
       <body>
         <noscript>
