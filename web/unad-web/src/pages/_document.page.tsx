da6c0f9b837e872c406cf6902efc930091f329c1
Stephen Collins <sgcollins@outlook.com>
2024-05-23 22:07:14 -0400

    added gtm

--- web/unad-web/src/pages/_document.page.tsx
+++ web/unad-web/src/pages/_document.page.tsx
@@ -5,6 +5,14 @@ export default function MyDocument() {
   return (
     <Html>
       <Head>
+        <Script
+          strategy="beforeInteractive"
+          id="gtm"
+        >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-NKZDQ2WJ');`}</Script>
         <meta name="theme-color" content="#111111" />
         <meta name="msapplication-navbutton-color" content="#111111" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -18,33 +26,11 @@ export default function MyDocument() {
         <link rel="apple-touch-icon" href="/img/favicon.webp" />
         <link rel="icon" type="image/x-icon" href="/img/favicon.webp" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <Script
-          async
-          strategy="beforeInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-L2SWP1F1TF"
-        ></Script>
-        <Script id="ga" strategy="beforeInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-L2SWP1F1TF');
-          `}
-        </Script>
-        <Script id="clarity" strategy="beforeInteractive">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "mbjvc7zj0r");
-          `}
-        </Script>
       </Head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PKLDLT6H"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NKZDQ2WJ"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
